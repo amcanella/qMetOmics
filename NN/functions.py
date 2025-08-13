9def835813bbdcffda569130557a53a14780ce40
alonso <alomc97@gmail.com>
2025-08-13 15:13:49 +0200

    Cleaning NN

--- NN/functions.py
+++ NN/functions.py
@@ -1,76 +1,48 @@
+import pandas as pd
+import numpy as np
+from simulator import process_data, Simulator
 
-import pandas as pd # type: ignore
-import numpy as np  # type: ignore
-import matplotlib.pyplot as plt  # type: ignore
+NFREQ = 22473 #32_768
+NMET = 66
 
-from simulator import process_data
-from simulator import Simulator
-
-p = process_data('/imppc/labs/jadlab/amoran/tbOmicsRepo/Metabo_tables_14.xlsx')
+p = process_data('path/to/database.xlsx')
 d = p.create_dict()
-
 s = Simulator(dictionary = d, met_data=p.met_data, clust_data = p.clust_data)
-print(f'\nthis is a test in functions: {p.met_data[0][6]}')
 
-def importSignals(value):
+def import_signals(value):
+  """Reads one pair of files (X,Y) for the given index and return DataFrames or arrays."""
+  dfx = pd.read_csv(f'samples/x_10000_66_{NFREQ}_{value}.csv')
 
-  dfx = pd.read_csv(f'/imppc/labs/jadlab/amoran/Metabolomics/Spectra_generator/spur/x_10000_66_{nfreq}_{value}.csv')
-
-  dfy = pd.read_csv(f'/imppc/labs/jadlab/amoran/Metabolomics/Spectra_generator/spur/y_met_10000_66_{nfreq}_{value}.csv')
-
-  #x = dfx.values
-  #y = dfy.values
+  dfy = pd.read_csv(f'samples/y_met_10000_66_{NFREQ}_{value}.csv')
 
   return dfx,dfy
 
-nfreq = 22473 #32_768 #52_234
-nmet = 66
-ndata = 10_000
-
-
 #TODO IMPORT A TXT
 def normalize_mets(y):
+  """Normalizes each metabolite column by its reference factor from p.met_data[:,6]."""
   rows = len(y)
-  yp = np.zeros([rows, nmet,1])
-  c=0 #track vector positionz
+  yp = np.zeros([rows, NMET,1])
+  c=0 #track vector positions
 
   for i in range(len(y[0])):
     if y[0][i]!= 0:
       yp[:,c] = (1/p.met_data[i][6])*y[:,i]
-      '''print(1/p.met_data[i][6])
-      print(c)
-      print(i)'''
-
       c+=1
     else:
       pass
-
-
-  '''yp[:, 0] =  1430 * y[:, 0]
-  yp[:, 1] = 100 * y[:, 2]
-  yp[:, 2] = 2000 * y[:, 3]
-  yp[:, 3] =  40 * y[:, 4]
-  yp[:, 4] = 115 * y[:, 6]
-  yp[:, 5] =  625 * y[:, 7]
-  yp[:, 6] =  3 * y[:, 8]
-  yp[:, 7] =  525 * y[:, 21]'''
 
   s = np.sum(yp, axis = 1).reshape((rows, 1,1))
   yp = yp / s
 
   return yp
 
-
-import scipy # type: ignore
-
-
-
-def customGenerator(lower, upper):
+def custom_generator(lower, upper):
+  """Loads and concatenates multiple indices into single X and Y arrays."""
   c = 0
   for j in range(lower, upper+1):
 
     print(f'\nLoading datasets...{j}')
-    dfx, dfy = importSignals(j)
+    dfx, dfy = import_signals(j)
 
     if c == 0:
       dfx_total = dfx
@@ -84,74 +56,3 @@ def customGenerator(lower, upper):
   y = dfy_total.values
 
   return x, y
-
-def plotLossFunc(current_datetime, history,run_name):
-  
-  plt.figure()
-  plt.xlabel('Epocs')
-  plt.ylabel('Amount loss')
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
-  epochs = range(1, len(loss)+1)
-  plt.plot(epochs, loss, 'y', label = 'Training loss')
-  plt.plot(epochs, val_loss, 'r', label = 'Validation loss')
-  title = 'Training and validation loss'
-  plt.title(f'{title}')
-  plt.legend()
-  #plt.ylim(0,0.000725)
-  #plt.xlim(0,54)
-  plot_name = f'kt_{current_datetime}_{title}_{run_name}'
-  plt.savefig(f'/imppc/labs/jadlab/amoran/tbOmicsRepo/figs/{plot_name}.png')
-  plt.show()
-
-def plotGraphs(current_datetime, y_t, y_test, pred_train, pred_test,run_name):
-
-  plt.figure()
-  plt.scatter(y_t[:,13], pred_train[:,13], c = 'r', label= 'Prediction training', alpha = 0.5)
-  plt.scatter(y_test[:,13], pred_test[:,13], c = 'b', label = 'Prediction test', alpha = 0.5)
-  plt.xlabel('real values')
-  plt.ylabel('predictions')
-  plt.legend()
-  title = f'glycine r = {scipy.stats.pearsonr(y_test[:,13], pred_test[:,13])[0]*100:.3f}%'
-  plt.title(title)
-  plot_name = f'kt_{current_datetime}_{title}_{run_name}'
-  plt.savefig(f'/imppc/labs/jadlab/amoran/tbOmicsRepo/figs/{plot_name}.png')
-  plt.show()
-  
-def plotGraphTraining(current_datetime, y_t, pred_train,run_name):
-
-  plt.figure()
-  plt.scatter(y_t[:,13], pred_train[:,13], c = 'r', label= 'Prediction training', alpha = 0.5)
-  plt.xlabel('real values')
-  plt.ylabel('predictions')
-  plt.legend()
-  y_t = y_t.reshape(len(y_t), nmet)
-  title = f'glycine training r = {scipy.stats.pearsonr(y_t[:,13], pred_train[:,13])[0]*100:.3f}%'
-  plt.title(title)
-  plot_name = f'kt_{current_datetime}_{title}_{run_name}'
-  plt.savefig(f'/imppc/labs/jadlab/amoran/tbOmicsRepo/figs/{plot_name}.png')
-  plt.show()
-
-
-def plotScatters(run_number, current_datetime,y_t, y_test, pred_train, pred_test,run_name):
-  plt.figure()
-  titles = ['hydroxy','acetate','alanine', 'aspartate', 'betaine', 'butyrate', 'creatine', 'creatinine', 'glucose', 'glutamate' ]
-  fig, ax = plt.subplots(3,4, figsize = (40,10))
-  c=0
-  for row in range(len(ax)):
-    for i in range(len(ax[0])):
-        ax[row, i].scatter(y_t[:,c], pred_train[:,c], c = 'r', label= 'Prediction training', alpha = 0.5) # type: ignore
-        ax[row, i].scatter(y_test[:,c], pred_test[:,c], c = 'b', label = 'Prediction test', alpha = 0.5) # type: ignore
-        ax[row, i].set_xlabel('real values')
-        ax[row, i].set_ylabel('predictions')
-        ax[row, i].legend()
-        ax[row, i].set_title(f'{d[c+1][1][0][1]} r = {scipy.stats.pearsonr(y_test[:,c], pred_test[:,c])[0]*100:.3f}%') # type: ignore
-        c+=1
-  plot_name = f'kt_{current_datetime}_scatters_{run_number}_{run_name}'
-  plt.savefig(f'/imppc/labs/jadlab/amoran/tbOmicsRepo/figs/{plot_name}.png')
-  plt.show()
-  
-  
-
-
-
--- NN/main.py
+++ NN/main.py
@@ -1,85 +1,47 @@
-#Summer 2024
-#Alonso Code
-
-# %% Import packages
-
-import tensorflow as tf # type: ignore
+"""
+Summer 2024
+Author: Alonso
+"""
+#Import packages
 import os
-
-from numba import cuda 
-device = cuda.get_current_device()
-device.reset()
-
-print('CURRENT DEVICE', device)
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
-
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print('\nImporting packages...')
 import numpy as np # type: ignore
-import pandas as pd
-#import keras_tuner
-import tensorflow as tf # type: ignore
-from tensorflow import keras # type: ignore
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv1D, Dropout, MaxPooling1D, BatchNormalization, GRU
-from tensorflow.keras import Model
-from keras.optimizers import Adam
 import time 
 import scipy
-
 import datetime
-import matplotlib.pyplot as plt
 
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-#from keras_tuner.tuners import RandomSearch
+import tensorflow as tf # type: ignore
+from tensorflow import keras # type: ignore
+from keras.optimizers import Adam
 from keras.models import load_model
-from tensorflow.keras.optimizers import Adam
-
-import os
 from sklearn.model_selection import train_test_split
 from scipy.stats import pearsonr
+from sklearn.metrics import r2_score
 
 from simulator import process_data
 from simulator import Simulator
-
-from sklearn.metrics import r2_score
-from sklearn.metrics import accuracy_score
-
 import functions 
 from modelQuant import buildModel
 
-import wandb
-from wandb.integration.keras import WandbCallback, WandbModelCheckpoint, WandbMetricsLogger 
-# Use wandb-core
-wandb.require("core")
-wandb.login()
-#0d86c681267d7dfd1f045e1852d14b0cf629d6ad
+
+NFREQ = 22473 #32_768
+NMET = 66
+ndata = 10_000
 
 # --------------------------------------------------------------------------------------------------------------------------------------
-
-p = process_data('/imppc/labs/jadlab/amoran/tbOmicsRepo/Metabo_tables_14.xlsx')
+#Import data from database
+p = process_data('C:/path/to/database.xlsx')
 d = p.create_dict()
 
 s = Simulator(dictionary = d, met_data=p.met_data, clust_data = p.clust_data)
 print(f'\nthis is a test: {p.met_data[0][6]}')
 
-#%%Import data
-nfreq = 22473 #32_768 #52_234
-nmet = 66
-ndata = 10_000
-#run_number=50
-#nnn= 13
 
-#for value in range(58, 61):
-# %%
+#Import simulated spectra in batches
 start = time.perf_counter()
 from_ = 19
 to_ = 27
-x , y = functions.customGenerator(from_, to_)
-print(x.shape)
+x , y = functions.custom_generator(from_, to_)
+
 samples, values = x.shape
 x = x.reshape(samples, values ,1) #not using in colab, is a tf convention, but otherwise obtaining error 
 y = y.reshape(samples, 67,1)
@@ -87,165 +49,73 @@ stop = time.perf_counter()
 
 timer = (stop - start) /60
 print(f'time to load data was {timer} minutes')
-# %%
-print (f'\nThe shape of the datasets are {x.shape, y.shape}')
+
 # %%Prepare data 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state=42) #90% training and 10% testing for many samples #75% 25% for low samples
 
 #Get the 10% of the training set for validation
 val_set = int(len(X_train)*0.1)
 
-#Coger 200 para este caso
 X_t = X_train[:-val_set]
 x_val = X_train[-val_set:]
 y_t = y_train[:-val_set]
 y_val = y_train[-val_set:]
 
-#SCALE DATA, OJO QUE NO SE SOBREESCRIBAN
-
+#Scale data
 y_t = functions.normalize_mets(y_t)
 y_val = functions.normalize_mets(y_val)
 y_test = functions.normalize_mets(y_test)
 
-print(f'The shape of X_t is {X_t.shape} and the shape of y_t is {y_t.shape}')
-# %%
+#Hyperparameters stored in dictionary
 best_hps = {'units0': 100,
-'conv_kernel0': 36, #9,
-#'units0B': 100,
-#'conv_kernel0B': 36,
+'conv_kernel0': 36,
 'dropout_conv_0': 0.1,
 'BN': False,
 'pool_size_0': 3,
 'units1': 200,
-'conv_kernel1': 20, #5,
-#'units1B': 200,
-#'conv_kernel1B': 20,
+'conv_kernel1': 20,
 'pool_size_1': 3,
 'units2': 500,
-'conv_kernel2': 12, #3,
+'conv_kernel2': 12,
 'dropout_conv_2': 0.1,
 'pool_size_2':2,
 'dense_units0': 200,
-'activation_dense1': 0.05, #0.01 mejor?
+'activation_dense1': 0.05,
 'learning_rate': 0.0001,
-'batch_size': 32, 
+'batch_size': 32,
 'batches': f'[{from_},{to_}]'}
-#, 'LSTM': 64}
-
-# Custom loss function with weighted MSE
-def weighted_mse_loss(y_true, y_pred):
-    # Define weights for each of the 66 outputs
-    weights = tf.ones([66])
-
-    # Update the value at position 33 (index 32) to 5.0
-    indices = [[47]]  # 33rd position is at index 32
-    updates = [10.0]
-    weights = tf.tensor_scatter_nd_update(weights, indices, updates)
-    
-    # Calculate MSE for each output
-    mse = tf.math.squared_difference(y_true, y_pred)
-    
-    # Multiply the MSE for each output by its corresponding weight
-    weighted_mse = mse * weights
-    
-    # Return the mean of the weighted MSE
-    return tf.reduce_mean(weighted_mse) #tensor
-
 
 
 # %%Start run 
-
-run_number = 270
-nnn=1
-run_name = f'SPURRR_customloss_10_{run_number}'
-#run_name = f'RNN_LSTM_SPURR_90'
-#run_name = f'EVAL_HIST_NEG005_{run_number}_{nnn}' 
-
-ckpt_path = f'weightsCkpt/modelQuant/{run_name}_ckpt.weights.h5'
-ckpt_dir = os.path.dirname(ckpt_path)
-path_previous_w = f'weightsCkpt/modelQuant/SPURRR_customloss_10_180_ckpt.weights.h5' #f'weightsCkpt/modelQuant/no_leaky_180_ckpt.weights.h5' #f'weightsCkpt/modelQuant/EVAL_HIST_NEG005_7{run_number}_{nnn}_ckpt.weights.h5'
-
-
-#run = wandb.init(project = 'trilasRNN', name = run_name, config = best_hps) #, id = 'r2fw818l', resume = 'allow') 
-run = wandb.init(project ='tbOmics',  name = run_name, config = best_hps) #, id = '9gy2oa9p', resume = 'allow')
-
-#Callbacks
-wandb_call = WandbCallback(monitor = 'val_loss', mode = 'min', save_model = False )
-logger = WandbMetricsLogger()
-cp = keras.callbacks.ModelCheckpoint(filepath = ckpt_path ,monitor = 'val_loss', save_best_only= True, mode= 'min', save_weights_only=True, verbose=1)
-lr_call = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=25)
-callbacks = [logger, cp]
-
-#model function was here 
-
-'''strategy = tf.distribute.MirroredStrategy()
-
-print('\nNumber of devices!!: {}'.format(strategy.num_replicas_in_sync))
-
-# Open a strategy scope
-with strategy.scope():'''
-#%%
-i_e = 1
-t_e = 150
-
-if i_e == 0:
-  first = buildModel(best_hps, nfreq, nmet)
-  #first.compile(keras.optimizers.Adam(learning_rate = best_hps['learning_rate']), loss = 'mean_squared_error', metrics = ['mae']) #E
-  print('FIRST MODEL ')
-else:
-  #pass
-  latest = tf.train.latest_checkpoint(ckpt_dir)
-  print(f'\nLatest model is : {latest}')
-  first = buildModel(best_hps, nfreq, nmet)
-  
-  first.load_weights(path_previous_w) #path_previous_w)
-  print(f'LOAD WEIGHTS at: {path_previous_w}') #path_previous_w')
-    
+model = buildModel(best_hps, NFREQ, NMET)
 
 # Print the model summary
-first.summary()
+model.summary()
 
-first.compile(Adam(learning_rate = best_hps['learning_rate']),
-                loss = weighted_mse_loss,
-                metrics = ['mae']) #E
-
-history = first.fit(X_t, y_t, epochs = t_e, initial_epoch = i_e, validation_data = (x_val, y_val), callbacks=callbacks, verbose = 1, shuffle = True, batch_size = best_hps['batch_size'] ) #shuffle?? callback for log y poder plotear en wandb??
-
-print(f'LOADING BEST CKPT...')
-latest = tf.train.latest_checkpoint(ckpt_dir)
-print(f'\nLatest model is : {latest}')
-print(f'loadinggg: {ckpt_path}')
-second = buildModel(best_hps, nfreq, nmet)
-second.load_weights(ckpt_path)
-second.compile(Adam(learning_rate = best_hps['learning_rate']),
-                loss = weighted_mse_loss,
+model.compile(optimizer = Adam(learning_rate = best_hps['learning_rate']),
+                loss = 'mse',
                 metrics = ['mae'])
 
+i_e = 0
+t_e = 150
+
+#Train the model
+history = model.fit(X_t, y_t, epochs = t_e, initial_epoch = i_e, validation_data = (x_val, y_val), verbose = 1, shuffle = True, batch_size = best_hps['batch_size'] )
+
+#Evaluate the training
 print('-----EVALUATION----')
-t_loss, t_mae = second.evaluate(X_t, y_t)
-test_loss, test_mae = second.evaluate(X_test, y_test)
+t_loss, t_mae = model.evaluate(X_t, y_t)
+test_loss, test_mae = model.evaluate(X_test, y_test)
 
+pred_train = model.predict(X_t)
+pred_test = model.predict(X_test)
 
-pred_train = second.predict(X_t)
-pred_test = second.predict(X_test)
-
-print(f'the sizes are y_test: {y_test.shape} and pred_test {pred_test.shape}')
-y_test = y_test.reshape(len(y_test), nmet)
-
-print(f'\nreshaping y_test...{y_test.shape}')
-
-
-
+y_test = y_test.reshape(len(y_test), NMET)
 r2 = r2_score(y_test, pred_test, multioutput='uniform_average')
-
 print(f'\nThe R2 obtained was: {r2*100:.3f} % \n')
 
-#To save r2 in wandb
-wandb.config['R2'] = r2 #you can use wandb.log too to log the data 
-
 l = []
-for i in range(nmet):
-
+for i in range(NMET):
   r = scipy.stats.pearsonr(y_test[:,i], pred_test[:,i])[0]
   print(f'met {i} with r: {r}')
   l.append(r)
@@ -255,33 +125,11 @@ print('Max corre is: ', np.max(l))
 print(f'the mean of the correlations is {np.mean(l)}')
 
 r_alanine_test = scipy.stats.pearsonr(y_test[:,2], pred_test[:,2])
-r_histidine = scipy.stats.pearsonr(y_test[:,47], pred_test[:,47])
 print(f'alanine correlation: {r_alanine_test[0]}')
 print(f'glicine correlation: {scipy.stats.pearsonr(y_test[:,13], pred_test[:,13])[0]}')
-wandb.config['r_ala'] = r_alanine_test[0]
-wandb.config['r_hist'] = r_histidine[0]
-wandb.config['r_mean'] = np.mean(l)
-
-
-current_datetime = current_datetime= datetime.datetime.now().strftime('%Y-%m-%d_%H_%M')
-
-functions.plotGraphs(current_datetime, y_t, y_test, pred_train, pred_test, run_name)
-functions.plotLossFunc(current_datetime, history, run_name)
-functions.plotScatters(run_number, current_datetime, y_t, y_test, pred_train, pred_test,run_name)
-functions.plotGraphTraining(current_datetime, y_t, pred_train,run_name)
 
 print('------------------------------------\n')
 
-#print(f'the keys ARE: {history.history.keys()}')  # Ensure 'loss' is one of the keys
-
-#HABRA QUE HACER KERAS API TAL VEZ 
-#print(history.history['loss'])
-model_path=f'/imppc/labs/jadlab/amoran/tbOmicsRepo/models/{run_name}_66_bestGLYBIEN_{r2:.3f}.h5' 
-second.save(model_path)
-second.save(os.path.join(wandb.run.dir, f"model_{nmet}_best_{r2:.3f}.keras")) #no se guardo!!!
-
-artifact = wandb.Artifact('model', type='model')
-artifact.add_file(model_path)
-wandb.log_artifact(artifact)
-
-run.finish()
+model_path='/path/to/model.h5'
+model.save(model_path)
+print('FINISHED!')
